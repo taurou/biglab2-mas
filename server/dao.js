24405eae6057917eaf7140f5d64a5bac6e753a74
s281605 <s281605@studenti.polito.it>
2021-05-12 20:24:27 +0200

    added dao.js

---
+++ server/dao.js
@@ -0,0 +1,160 @@
+'use strict';
+/* Data Access Object (DAO) module for accessing courses and exams */
+
+const sqlite = require('sqlite3');
+
+// open the database
+const db = new sqlite.Database('tasks.db', (err) => {
+  if(err) throw err;
+});
+
+// get all courses
+exports.listTasks = () => {
+  return new Promise((resolve, reject) => {
+    const sql = 'SELECT * FROM tasks';
+    db.all(sql, [], (err, rows) => {
+      if (err) {
+        reject(err);
+        return;
+      }
+      const tasks = rows.map((t) => ({ id: t.id, description: t.description, important: t.important, deadline : t.deadline, private: t.private }));
+      resolve(tasks);
+    });
+  });
+};
+
+// get important tasks
+exports.getImportantTasks = () => {
+  return new Promise((resolve, reject) => {
+    const sql = 'SELECT * FROM tasks WHERE important = 1';
+    db.all(sql, [], (err, rows) => {
+      if (err) {
+        reject(err);
+        return;
+      }
+      const tasks = rows.map((t) => ({ id: t.id, description: t.description, important: t.important, deadline : t.deadline, private: t.private }));
+      resolve(tasks);
+    });
+  });
+};
+
+// get private tasks
+exports.getPrivateTasks = () => {
+  return new Promise((resolve, reject) => {
+    const sql = 'SELECT * FROM tasks WHERE private = 1';
+    db.all(sql, [], (err, rows) => {
+      if (err) {
+        reject(err);
+        return;
+      }
+      const tasks = rows.map((t) => ({ id: t.id, description: t.description, important: t.important, deadline : t.deadline, private: t.private }));
+      resolve(tasks);
+    });
+  });
+};
+
+
+
+exports.getTaskByDeadline = (deadline) => {
+  return new Promise((resolve, reject) => {
+    const sql = 'SELECT * FROM tasks WHERE deadline=?';
+    db.get(sql, [deadline], (err, row) => {
+      if (err) {
+        reject(err);
+        return;
+      }
+      if (row == undefined) {
+        reject({error: 'Deadline not found.'});
+      } else {
+        const task = { id: t.id, description: t.description, important: t.important, deadline : t.deadline, private: t.private  };
+        resolve(task);
+      }
+    });
+  });
+};
+
+
+
+exports.getTaskById = (id) => {
+  return new Promise((resolve, reject) => {
+    const sql = 'SELECT * FROM tasks WHERE id=?';
+    db.get(sql, [id], (err, row) => {
+      if (err) {
+        reject(err);
+        return;
+      }
+      if (row == undefined) {
+        reject({error: 'Id not found.'});
+      } else {
+        const task = { id: t.id, description: t.description, important: t.important, deadline : t.deadline, private: t.private  };
+        resolve(task);
+      }
+    });
+  });
+};
+
+// // get all exams
+// exports.listExams = () => {
+//   return new Promise((resolve, reject) => {
+//     const sql = 'SELECT coursecode, score, date FROM exam';
+
+//     db.all(sql, (err, rows) => {
+//       if (err) {
+//         reject(err);
+//         return;
+//       }
+
+//       const exams = rows.map((e) => (
+//         {
+//           code: e.coursecode,
+//           score: e.score,
+//           date: e.date,
+//         }));
+
+//       resolve(exams);
+//     });
+//   });
+// };
+
+// add a new task
+exports.createTask = (task) => {
+  return new Promise((resolve, reject) => {
+    const sql = 'INSERT INTO tasks(description, important, deadline, private) VALUES(?, ?, DATETIME(?), ?)';
+    //TODO fare controlli dei booleani
+    db.run(sql, [task.description, task.important, task.deadline, task.private], function (err) {
+      if (err) {
+        reject(err);
+        return;
+      }
+      resolve(this.lastID);
+    });
+  });
+};
+
+// update an existing task
+exports.updateTask = (task) => {
+  return new Promise((resolve, reject) => {
+    const sql = 'UPDATE tasks SET description=?, important=?, deadline=DATETIME(?), private=? WHERE id = ?';
+    db.run(sql, [task.description, task.important, task.deadline, task.private, task.id], function (err) {
+      if (err) {
+        reject(err);
+        return;
+      }
+      resolve(this.lastID);
+    });
+  });
+};
+
+// delete an existing task
+exports.deleteTask = (task_id) => {
+  return new Promise((resolve, reject) => {
+    const sql = 'DELETE FROM tasks WHERE id = ?';
+    db.run(sql, [task_id], (err) => {
+      if (err) {
+        reject(err);
+        return;
+      } else
+        resolve(null);
+    });
+  });
+}
