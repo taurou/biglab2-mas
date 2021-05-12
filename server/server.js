f30102422bbbeea09a92c1ab57e5e7cbcc3e9d25
s281605 <s281605@studenti.polito.it>
2021-05-12 20:23:39 +0200

    Added some sql code

--- server/server.js
+++ server/server.js
@@ -2,6 +2,11 @@ const express = require('express');
 
 const PORT = 3001;
 
+
 app = new express();
+
+
+app.get('/', (req,res) => res.send('ADORO'));
+
 
 app.listen(PORT, ()=>console.log(`Server running on http://localhost:${PORT}/`));
